39d0bdae8dd9b0a6665b3ac4cdedbb2137f74f0a
Simon Lammers <sim.lammers@gmail.com>
2026-06-11 22:34:18 +0200

    rough line function

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, const TGAColor &color, TGAImage &framebuffer) {
+    for (float t = 0; t < 1; t += 0.01) {
+        int x = ax + (bx - ax) * t;
+        int y = ay + (by - ay) * t;
+        framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -19,6 +27,10 @@ int main(int argc, char** argv) {
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
     framebuffer.set(cx, cy, white);
+
+    line(ax, ay, bx, by, green, framebuffer);
+    line(bx, by, cx, cy, red,   framebuffer);
+    line(cx, cy, ax, ay, blue,  framebuffer);
 
     return framebuffer.write_tga_file("framebuffer.tga");
 }
